c6403eb425a32c8989f13d136bade0eed6e47ad7
Shadi Akiki <shadiakiki1986@gmail.com>
2016-09-27 08:05:01 +0300

    add security field to config

--- README.md
+++ README.md
@@ -7,20 +7,30 @@ Install: `composer require shadiakiki1986/swiftmailer-wrapper:dev-master`
 Use:
 ```php
 require_once __DIR__.'/vendor/autoload.php';
+
 \SwiftmailerWrapper\Utils::mail_attachment(
+
+  // attachments
   array("/path/to/file1","/path/to/file2"),
+
+  // to, from, reply emails
   "to@email.com",
   "from@email.com",
   "From Name",
   "reply@email.com",
+
+  // subject, body
   "This is a subject",
   "This is a message. It <i>supports</i> html.",
+
+  // config array
   array(
     "host"=>"smtp.server.com",
     "port"=>"123",
     "username"=>"myUser",
     "password"=>"pizza"
   )
+
 );
 ```
 
@@ -28,3 +38,5 @@ Files can be renamed in attachment by passing files parameter as follows:
 ```php
   array("newName1"=>"/path/to/file1","newName2"=>"/path/to/file2"),
 ```
+
+To use [encrypted smtp](http://swiftmailer.org/docs/sending.html#encrypted-smtp), add `security` key to the config array. Valid values are: `false, ssl, tls`
--- src/SwiftmailerWrapper/Utils.php
+++ src/SwiftmailerWrapper/Utils.php
@@ -6,8 +6,16 @@ class Utils {
   public static function mail_attachment($files, $mailto, $from_mail, $from_name, $replyto, $subject, $message, $config) {
     if(!class_exists("\Swift_Message")) throw new Exception("Email support not installed on server. Aborting");
 
+    // required entries
     $missingConfigKeys = array_diff(array("host","port","username","password"),array_keys($config));
     if(count($missingConfigKeys)>0) throw new Exception("Missing config keys: ".implode(", ",$missingConfigKeys));
+
+    // optional entries
+    if(!array_key_exists('security',$config)) {
+      $config['security']=false;
+    } else {
+      if(!in_array($config['security'],array('ssl','tls'))) throw new \Exception("Invalid security option: ".$config['security']);
+    }
 
     $message = \Swift_Message::newInstance()
         ->setSubject($subject)
@@ -30,6 +38,11 @@ class Utils {
     $transport = \Swift_SmtpTransport::newInstance($config["host"],$config["port"])
         ->setUsername($config["username"])
         ->setPassword($config["password"]);
+
+    // http://swiftmailer.org/docs/sending.html#encrypted-smtp
+    // https://github.com/swiftmailer/swiftmailer/blob/fffbc0e2a7e376dbb0a4b5f2ff6847330f20ccf9/lib/classes/Swift/SmtpTransport.php#L42
+    if(!!$config['security']) $transport->setEncryption($config['security']);
+
     $mailer = \Swift_Mailer::newInstance($transport);
     return $mailer->send($message);
   }
